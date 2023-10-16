4544d488485922a9b627c88786bbc1ff7877516e
chakri-143 <chakribontha9@gmail.com>
2023-10-16 23:45:31 +0530

    complete react app

--- App.js
+++ App.js
@@ -1,18 +1,3 @@
-const parent=React.createElement(
-    'div',
-{'id':'parent'},[
-    React.createElement('div',{'id':'child'},[
-            React.createElement('h1',{},'i am h1 tag'),
-            React.createElement('h1',{},'i am h2 tag')
-      ]),
-   React.createElement('div',{'id':'child'},[
-        React.createElement('h1',{},'i am hsrkdufrehi tag'),
-        React.createElement('h1',{},'i am hsjfhssdj tag')
-     ])
-]
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-)
-
-console.log(parent)
-const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent)
